34000e7a761117e8ab2c335c12dda0dbd556428e
Cyrus <cdbalsara@icloud.com>
2016-11-08 16:30:14 +0000

    blank app called Word Counter up and running

---
+++ app/src/main/java/com/example/user/wordcounter/WordCount.java
@@ -0,0 +1,25 @@
+package com.example.user.wordcounter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 08/11/2016.
+ */
+
+public class WordCount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+
+
+
+
+}
---
+++ app/src/test/java/com/example/user/wordcounter/WordCountTest.java
@@ -0,0 +1,8 @@
+package com.example.user.wordcounter;
+
+/**
+ * Created by user on 08/11/2016.
+ */
+
+public class WordCountTest {
+}
