0e459b0bdf09d93d3ab042a1170bcb51d3a60ea5
Cyrus <cdbalsara@icloud.com>
2016-11-08 17:04:20 +0000

    added edit text control to app

--- app/src/main/java/com/example/user/wordcounter/WordCount.java
+++ app/src/main/java/com/example/user/wordcounter/WordCount.java
@@ -2,6 +2,7 @@ package com.example.user.wordcounter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
  * Created by user on 08/11/2016.
@@ -11,6 +12,7 @@ public class WordCount extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
+        Log.d("WordCount", "onCreate is up and running properly!");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
